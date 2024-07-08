220499563b2915f6a1ce028b33987bc41cea1c3d
Midoria7 <buptxmh@bupt.edu.cn>
2024-07-08 15:09:25 +0800

    修改视频直接用后端api取消websocket

--- src/components/VideoMonitor.tsx
+++ src/components/VideoMonitor.tsx
@@ -1,20 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpRightAndDownLeftFromCenter, faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons';
 
 const VideoMonitor: React.FC = () => {
-  const [frame, setFrame] = useState<string>('');
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false);  // 跟踪全屏状态
   const videoContainerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const ws = new WebSocket('ws://192.168.252.106:5678');
-    ws.onopen = () => console.log('Connected to the websocket');
-    ws.onmessage = (event) => setFrame(`data:image/jpeg;base64,${event.data}`);
-    ws.onerror = (error) => console.error('WebSocket error:', error);
-    ws.onclose = () => console.log('WebSocket connection closed');
-    return () => ws.close();
-  }, []);
 
   useEffect(() => {
     const handleFullScreenChange = () => {
@@ -62,11 +52,7 @@ const VideoMonitor: React.FC = () => {
     <div className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center w-[800px] relative">
       <h2 className="text-2xl font-semibold mb-4 mt-1 text-gray-800">监控画面</h2>
       <div ref={videoContainerRef} className="video-container flex justify-center items-center bg-black rounded h-full w-full relative">
-        {frame ? (
-          <img src={frame} alt="Video Stream" className="max-w-full max-h-full object-cover" />
-        ) : (
-          <p className="text-white">等待视频数据...</p>
-        )}
+        <img src="http://192.168.252.106:5000/video_feed" alt="Video Stream" className="max-w-full max-h-full object-cover" />
         <button onClick={toggleFullScreen} className="absolute bottom-5 right-5 w-10 h-10 bg-white text-black rounded hover:bg-gray-200">
           <FontAwesomeIcon icon={isFullScreen ? faDownLeftAndUpRightToCenter : faUpRightAndDownLeftFromCenter} />
         </button>
